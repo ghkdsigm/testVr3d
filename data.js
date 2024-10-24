80442b78decad05f996e8e637347a730a8ddd594
DONGWHA\D2301202 <sh.hwang@dongwha.com>
2024-10-24 16:34:21 +0900

    update

--- data.js
+++ data.js
@@ -1,7 +1,55 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-",
+      "id": "0-sphere",
+      "name": "sphere",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0.7742052737377243,
+        "pitch": 0.08469598073368978,
+        "fov": 1.321528281092935
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.7742052737377243,
+          "pitch": 0.08469598073368978,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7694600604486208,
+          "pitch": 0.04136755000090808,
+          "title": "ㄴㄴㄴㄴㄴㄴㄴ",
+          "text": "ㅁㅇㄴㄹ"
+        }
+      ]
+    },
+    {
+      "id": "1-",
       "name": "",
       "levels": [
         {
@@ -38,7 +86,7 @@ var APP_DATA = {
   ],
   "name": "Project Title",
   "settings": {
-    "mouseViewMode": "drag",
+    "mouseViewMode": "qtvr",
     "autorotateEnabled": true,
     "fullscreenButton": false,
     "viewControlButtons": false
